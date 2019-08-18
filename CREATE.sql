9dea597c768f5626e5827e19a0b82526332416be
GustavoSma <42275661+GustavoSma@users.noreply.github.com>
2019-08-18 20:23:42 -0300

    Add files via upload

---
+++ CREATE.sql
@@ -0,0 +1,12 @@
+create database Proj;
+
+create table judoca(
+	j_id int not null,
+	j_name VARCHAR(255) not null,
+	j_altura float not null,
+	j_peso float not null,
+	primary key(j_id)
+);
+
+create table professor();
+create table escolas();
